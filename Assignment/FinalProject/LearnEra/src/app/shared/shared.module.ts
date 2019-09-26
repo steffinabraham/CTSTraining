97e032613f01757c3439ad0f1c0fc9f466311493
Steffin Abraham <steffinabraham09@gmail.com>
2019-09-26 17:45:08 +0530

    EOD-26th Sep

--- Assignment/FinalProject/LearnEra/src/app/shared/shared.module.ts
+++ Assignment/FinalProject/LearnEra/src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NavComponent],
   imports: [
     CommonModule
   ]
